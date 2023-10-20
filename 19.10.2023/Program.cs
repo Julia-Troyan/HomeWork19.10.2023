d5c7f67a2e71df56161de8581f41e9fe2a01f8d6
Julia-Tryan <uliana.troyan@mail.ru>
2023-10-20 22:07:21 +0300

    Решение второй задачи на автотесте

--- 19.10.2023/Program.cs
+++ 19.10.2023/Program.cs
@@ -55,34 +55,47 @@
 // принимает на вход координаты двух точек pointA и pointB в виде массива целых
 // чисел, и возвращает расстояние между ними в 3D пространстве.
 
-class Answer
+
+using System;
+
+public class Answer
 {
-    public static double DistanceBetweenPoints(int[] pointA, int[] pointB)
+    private static double Length(int[] pointA, int[] pointB)
     {
         if (pointA.Length != 3 || pointB.Length != 3)
         {
             throw new ArgumentException("Точки должны быть представлены тройками координат");
         }
 
-        int x1 = pointA[0];
-        int y1 = pointA[1];
-        int z1 = pointA[2];
-
-        int x2 = pointB[0];
-        int y2 = pointB[1];
-        int z2 = pointB[2];
-
-        double distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
-
+        double distance = Math.Sqrt(pointA.Zip(pointB, (a, b) => Math.Pow(a - b, 2)).Sum());
         return distance;
     }
 
-    static void Main()
+    public static void Main(string[] args)
     {
-        int[] point1 = { 2, 1, 7 };
-        int[] point2 = { 1, -1, 9 };
+        int x1, x2, x3, y1, y2, y3;
 
-        double result = DistanceBetweenPoints(point1, point2);
-        Console.WriteLine($"Расстояние между точкой 1 и точкой 2: {result:F2}");
+        if (args.Length >= 6)
+        {
+            x1 = int.Parse(args[0]);
+            x2 = int.Parse(args[1]);
+            x3 = int.Parse(args[2]);
+            y1 = int.Parse(args[3]);
+            y2 = int.Parse(args[4]);
+            y3 = int.Parse(args[5]);
+        }
+        else
+        {
+            // Здесь вы можете поменять значения для отправки кода на Выполнение
+            x1 = 3;
+            x2 = 6;
+            x3 = 8;
+            y1 = 2;
+            y2 = 1;
+            y3 = -7;
+        }
+
+        double result = Length(new int[] { x1, x2, x3 }, new int[] { y1, y2, y3 });
+        Console.WriteLine($"{result:F2}");
     }
 }
