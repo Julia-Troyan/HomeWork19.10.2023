bf4fde32b657337ba48aa89d5adfdfe8989b9574
Julia-Tryan <uliana.troyan@mail.ru>
2023-10-20 22:44:45 +0300

    Решение третьей задачи на автотесте

--- 19.10.2023/Program.cs
+++ 19.10.2023/Program.cs
@@ -56,46 +56,85 @@
 // чисел, и возвращает расстояние между ними в 3D пространстве.
 
 
-using System;
+// using System;
+
+// public class Answer
+// {
+//     private static double Length(int[] pointA, int[] pointB)
+//     {
+//         if (pointA.Length != 3 || pointB.Length != 3)
+//         {
+//             throw new ArgumentException("Точки должны быть представлены тройками координат");
+//         }
+
+//         double distance = Math.Sqrt(pointA.Zip(pointB, (a, b) => Math.Pow(a - b, 2)).Sum());
+//         return distance;
+//     }
+
+//     public static void Main(string[] args)
+//     {
+//         int x1, x2, x3, y1, y2, y3;
+
+//         if (args.Length >= 6)
+//         {
+//             x1 = int.Parse(args[0]);
+//             x2 = int.Parse(args[1]);
+//             x3 = int.Parse(args[2]);
+//             y1 = int.Parse(args[3]);
+//             y2 = int.Parse(args[4]);
+//             y3 = int.Parse(args[5]);
+//         }
+//         else
+//         {
+//             x1 = 3;
+//             x2 = 6;
+//             x3 = 8;
+//             y1 = 2;
+//             y2 = 1;
+//             y3 = -7;
+//         }
+
+//         double result = Length(new int[] { x1, x2, x3 }, new int[] { y1, y2, y3 });
+//         Console.WriteLine($"{result:F2}");
+//     }
+// }
+
+// Внутри класса Answer напишите метод ShowCube, 
+// который принимает на вход число
+// (N) и выводит таблицу кубов чисел от 1 до N 
+// (включительно) каждое на новой строке.
 
 public class Answer
 {
-    private static double Length(int[] pointA, int[] pointB)
+   static void ShowCube(int N)
     {
-        if (pointA.Length != 3 || pointB.Length != 3)
+      // Введите свое решение ниже
+        if (N < 1)
         {
-            throw new ArgumentException("Точки должны быть представлены тройками координат");
+            Console.WriteLine("N должно быть больше или равно 1.");
+            return;
         }
 
-        double distance = Math.Sqrt(pointA.Zip(pointB, (a, b) => Math.Pow(a - b, 2)).Sum());
-        return distance;
-    }
-
-    public static void Main(string[] args)
-    {
-        int x1, x2, x3, y1, y2, y3;
-
-        if (args.Length >= 6)
+        for (int i = 1; i <= N; i++)
         {
-            x1 = int.Parse(args[0]);
-            x2 = int.Parse(args[1]);
-            x3 = int.Parse(args[2]);
-            y1 = int.Parse(args[3]);
-            y2 = int.Parse(args[4]);
-            y3 = int.Parse(args[5]);
+            int cube = i * i * i;
+            Console.WriteLine($"{i} в кубе равно {cube}");
         }
-        else
-        {
-            // Здесь вы можете поменять значения для отправки кода на Выполнение
-            x1 = 3;
-            x2 = 6;
-            x3 = 8;
-            y1 = 2;
-            y2 = 1;
-            y3 = -7;
+}
+
+
+  // Не удаляйте и не меняйте метод Main! 
+      static public void Main(string[] args) {
+        int N;
+
+        if (args.Length >= 1) {
+            N = int.Parse(args[0]);
+        } else {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+            N = 6;
         }
 
-        double result = Length(new int[] { x1, x2, x3 }, new int[] { y1, y2, y3 });
-        Console.WriteLine($"{result:F2}");
+        // Не удаляйте строки ниже
+        ShowCube(N);
     }
 }
